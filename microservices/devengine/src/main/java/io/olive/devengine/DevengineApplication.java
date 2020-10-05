9934eeb6224c775e326700287808b2b42b9a275e
Nishant Hulwan <nishanthulwan47@gmail.com>
2020-10-04 20:16:59 -0400

    initial commit

---
+++ microservices/devengine/src/main/java/io/olive/devengine/DevengineApplication.java
@@ -0,0 +1,13 @@
+package io.olive.devengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevengineApplication.class, args);
+	}
+
+}
---
+++ microservices/devengine/src/test/java/io/olive/devengine/DevengineApplicationTests.java
@@ -0,0 +1,13 @@
+package io.olive.devengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevengineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
